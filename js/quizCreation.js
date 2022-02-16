8ca7c7828282a37ada08c8a0c1395f4d85131d39
heusyfelype <felype.heusy@gmail.com>
2022-02-16 17:07:14 -0300

    Criada a funcao de clicar no botao de criar novo Quizz

---
+++ js/quizCreation.js
@@ -0,0 +1,12 @@
+document.querySelector(".quizzes-criados ion-icon").onclick = function(){chamarTelaCriarQuizz()}
+
+function chamarTelaCriarQuizz(){
+    console.log('esta chamando a funcao')
+    let telaUmPontoUM = document.querySelector("#tela1_1");
+    let telaUmPontoDoin = document.querySelector("#tela1_2");
+    let telasTresPontoUmETresPontoDois = document.querySelector("#tela3_1-2");
+
+    telaUmPontoUM.classList.toggle("esconder");
+    telaUmPontoDoin.classList.toggle("esconder");
+    telasTresPontoUmETresPontoDois.classList.toggle("esconder");
+}
