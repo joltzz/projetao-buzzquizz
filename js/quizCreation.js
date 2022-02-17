e171d9bf167fae531d0dfde73299060ef97c2020
heusyfelype <felype.heusy@gmail.com>
2022-02-16 21:01:07 -0300

    funcoes adicionar pergunta e adicionar nível arrumadas

--- js/quizCreation.js
+++ js/quizCreation.js
@@ -1,6 +1,6 @@
 document.querySelector(".quizzes-criados ion-icon").onclick = function () { chamarTelaCriarQuizz() }
-document.querySelector("#pergunta2 img").onclick=function(){ adicionaPergunta2() }
-document.querySelector("#pergunta3 img").onclick=function(){ adicionaPergunta3() }
+document.querySelector("#pergunta2 img").onclick = function () { adicionaPergunta2() }
+document.querySelector("#pergunta3 img").onclick = function () { adicionaPergunta3() }
 
 function chamarTelaCriarQuizz() {
     let telaUmPontoUM = document.querySelector("#tela1_1");
@@ -22,7 +22,7 @@ function chamarTelaPerguntasDoQuiz() {
 }
 
 document.querySelector(".criacao-perguntas button").onclick = function () { chamarTelaDecidirNiveis() }
-function chamarTelaDecidirNiveis(){
+function chamarTelaDecidirNiveis() {
     let teladecidirNiveis = document.querySelector("#tela3_3");
     let telaCriacaoPerguntas = document.querySelector(".criacao-perguntas");
 
@@ -30,23 +30,31 @@ function chamarTelaDecidirNiveis(){
     telaCriacaoPerguntas.classList.toggle("esconder");
 }
 
-document.querySelector("#nivel2").onclick = function(){adicionarnivel()};
-document.querySelector("#nivel3").onclick = function(){adicionarnivel()};
+document.querySelector(".info-nivel button").onclick = function () { finalizarQuizz() }
+function finalizarQuizz() {
+    let teladecidirNiveis = document.querySelector("#tela3_3");
+    let telaQuizzPronto = document.querySelector(".finalizar-Quizz");
 
-function adicionaPergunta2(){
-    let adicionaPergunta=document.querySelector("#pergunta2");
-    adicionaPergunta.innerHTML=""
-    adicionaPergunta.innerHTML+=`
+    telaQuizzPronto.classList.toggle("esconder");
+    teladecidirNiveis.classList.toggle("esconder");
+}
+
+
+document.querySelector("#pergunta2").onclick = function () { adicionarPerguntas("#pergunta2", 2) };
+document.querySelector("#pergunta3").onclick = function () { adicionarPerguntas("#pergunta3", 3) };
+
+function adicionarPerguntas(pergunta, numero) {
+    let localAdicionarPergunta = document.querySelector(pergunta);
+    localAdicionarPergunta.innerHTML = ""
+    localAdicionarPergunta.innerHTML += `
         <div class="inputs">
                 <article class="pergunta">
-                    <h1>Pergunta 2</h1>
+                    <h1> Pergunta ${numero} </h1>
                     <input type="text" placeholder="Texto da pergunta">
                     <input type="text" placeholder="Cor de fundo da pergunta">
-
                     <h1 style="margin-top: 28px;">Resposta correta</h1>
                     <input type="text" placeholder="Resposta correta">
                     <input type="text" placeholder="URL da imagem">
-
                     <h1 style="margin-top: 28px;">Respostas incorretas</h1>
                     <input type="text" placeholder="Resposta incorreta 1">
                     <input type="text" placeholder="URL da imagem 1">
@@ -55,44 +63,21 @@ function adicionaPergunta2(){
                     <input type="text" placeholder="Resposta incorreta 3">
                     <input type="text" placeholder="URL da imagem 3">
                 </article>
-            </div>`}
+            </div>`
+    document.querySelector(pergunta).onclick = ""
+}
 
-function adicionaPergunta3(){
-    let adicionaPergunta=document.querySelector("#pergunta3");
-    adicionaPergunta.innerHTML=""
-    adicionaPergunta.innerHTML+=`
-        <div class="inputs">
-                <article class="pergunta">
-                    <h1>Pergunta 3</h1>
-                    <input type="text" placeholder="Texto da pergunta">
-                    <input type="text" placeholder="Cor de fundo da pergunta">
-
-                    <h1 style="margin-top: 28px;">Resposta correta</h1>
-                    <input type="text" placeholder="Resposta correta">
-                    <input type="text" placeholder="URL da imagem">
-
-                    <h1 style="margin-top: 28px;">Respostas incorretas</h1>
-                    <input type="text" placeholder="Resposta incorreta 1">
-                    <input type="text" placeholder="URL da imagem 1">
-                    <input type="text" placeholder="Resposta incorreta 2">
-                    <input type="text" placeholder="URL da imagem 2">
-                    <input type="text" placeholder="Resposta incorreta 3">
-                    <input type="text" placeholder="URL da imagem 3">
-                </article>
-            </div>`}
-
-function adicionarnivel(){
-    console.log("a funcao esta sendo chamada")
-    let conteudo = document.querySelector("#nivel2");
-    console.log(conteudo)
+document.querySelector("#nivel2").onclick = function () { adicionarnivel("#nivel2") };
+document.querySelector("#nivel3").onclick = function () { adicionarnivel("#nivel3") };
+function adicionarnivel(nivel) {
+    let conteudo = document.querySelector(nivel);
     conteudo.innerHTML += `
     <div class="inputs">
-                <p>Nível 1</p>
                 <input type="text" placeholder="Título do nível">
                 <input type="text" placeholder="% de acerto mínima">
                 <input type="text" placeholder="URL da imagem do nível">
                 <textarea name="" id="" class="area-de-texto" placeholder="Descrição do nível"></textarea>
             </div>
     `
-    console.log(conteudo)
+    document.querySelector(nivel).onclick = ""
 }
