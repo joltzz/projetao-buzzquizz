e4b129ec3104852600e9de6d5bda501f52009d59
joltzz <guijsilva02@gmail.com>
2022-02-21 16:31:49 -0300

    projeto finalizado, restando ajustar a imagem do resultado tela 2

--- js/quizPage.js
+++ js/quizPage.js
@@ -4,13 +4,16 @@ let quizData = [];
 let newQuiz = [];
 let idHolder = null;
 let saveData;
+let pass = 0;
+let result;
 let lock = false;
 let error = false;
+
 
 // requisição para buscar todos os quizzes
 let promisse = axios.get(API_BUZZQUIZZ);
 promisse.then(renderQuizzes);
-promisse.catch(mensagemDeErro)
+promisse.catch( function (){alert("Erro ao recarregar os Quizzes.");  window.location.reload(); } )
 
 
 // função para renderizar os quizzes retirados da api
@@ -21,7 +24,7 @@ function renderQuizzes(resposta) {
 
     for (let i = 0; i < quizData.length; i++) {
         quizList.innerHTML += `
-        <div onclick="irParaQuizz(this)" class="quiz-main" id="${quizData[i].id}">
+        <div onclick="irParaQuizz(this)" class="quiz-main" id="${quizData[i].id}" data-identifier="quizz-card">
             <img src="${quizData[i].image}" alt="modelo">
             <div class="backgound-gradiente"></div>
             <p class="description">${quizData[i].title}</p>
@@ -29,12 +32,6 @@ function renderQuizzes(resposta) {
         `;
     }
 }
-
-function mensagemDeErro(){
-    alert("Esta dando erro na requisicao")
-}
-
-
 function irParaQuizz(acessarQuizz){
     idHolder = acessarQuizz.id;
     // console.log(idHolder);
@@ -52,29 +49,164 @@ function chamaPaginaDois(resposta) {
     document.querySelector("#tela1_2").classList.add("esconder");
     document.querySelector("#tela2").classList.remove("esconder");
     
-    renderBanner(); // função para renderizar banner
-    renderQuestions();// função para renderizar perguntas
+    renderizaBanner(); 
+    renderizaPerguntas();
+    document.querySelector(".pagina-de-um-quizz__titulo-de-um-quizz").scrollIntoView({block:"start"});
 }
 
-function renderBanner(){
-    //renderiza a imagem da tela 2
+function renderizaBanner(){
+    const bannerImage = document.querySelector(".pagina-de-um-quizz__titulo-de-um-quizz");
+    bannerImage.innerHTML = `
+    <img src="${saveData.image}">
+    <div class="quiz-page-description">${saveData.title}</div>
+    `;
 }
 
-function renderQuestions(){
-    //renderiza as perguntas da tela 2
+function renderizaPerguntas(){
+    let i = 0;
+    const questions = saveData.questions
+    const quizQuestions = document.querySelector(".box-de-cada-pergunta__a-pergunta");
+    questions.forEach(question => {
+        let color = question.color;
+        // console.log(color);
+        // console.log(question);
+        quizQuestions.innerHTML += `
+        <section class="box-de-cada-pergunta__a-pergunta">
+        <div class="question-card${i}">
+                <div class="question-description" id="${i}" data-identifier="question"><h4>${question.title}<h4></div>
+                <div class="all-answers">${renderizaRespostas(question)}</div>
+            </div>
+        </section>
+        `;
+        document.getElementById(`${i}`).style.backgroundColor=color;
+        i++;
+        // console.log(quizQuestions);
+    });
 }
 
+function renderizaRespostas(question){
+    const armazenaRespostas = question.answers
+    const answers = armazenaRespostas.sort(() => Math.random() - 0.5);
+    console.log(answers);
+    let quizAnswers = "";
+    answers.forEach(answer => {
+        quizAnswers += `
+        <div onclick="escolheResposta(this)" class="answer ${answer.isCorrectAnswer}" data-identifier="answer">
+            <img src="${answer.image}">
+            <h4>${answer.text}</h4>
+        </div>
+        `;
+    });
+    // console.log(quizAnswers)
+    return quizAnswers;
+}
 
+function escolheResposta(answerElement) {
+    // console.log(answerElement);
+    answerElement.classList.add("selecionado")
+    let parentElement = answerElement.parentNode;
+    let selecionaResposta = parentElement.querySelectorAll(".answer")
 
+    selecionaResposta.forEach(answer => {
+        let elementSelect = answer.classList.contains("selecionado")
+        if (elementSelect === false) {
+            answer.classList.add("esbranquicado");
+            answer.setAttribute("onclick", "");
+        } else {
+            answer.setAttribute("onclick", "");
+        }
+    });
+    answerResult(answerElement);
+    // função para scrolar para a próxima pergunta 
+    setTimeout(() => {
+        pass += 1;
+        divcardQuestao = `.question-card${pass}`;
+        scrollElement = document.querySelector(divcardQuestao);
 
+        if (scrollElement === null) {
+            renderizaResultado();
+            const resultQuiz = document.querySelector(".resultado-final");
+            resultQuiz.classList.remove("esconder")
+            resultQuiz.scrollIntoView({block: "center", behavior: "smooth"})
+            console.log("testeeeeeeeeeeee");
+        } else {
+        scrollElement.scrollIntoView({block: "center", behavior: "smooth"});
+        console.log(scrollElement);
+        }
+    }, 2000);
+}
 
+function answerResult(answerElement){
+    let parentElement = answerElement.parentNode;
+    let selectAnswer = parentElement.querySelectorAll(".answer h4");
+    console.log(selectAnswer);
 
+    let ifTrue = parentElement.querySelector(".true h4");
+    ifTrue.style.color="green";
+
+    let ifFalse = parentElement.querySelectorAll(".false h4"); 
+    ifFalse.forEach(answer => {
+        answer.style.color="red";
+    });
+}
+
+//renderiza o resultado do quiz
+function renderizaResultado(){
+    apareceBotoes()
+    let perguntasLength = saveData.questions.length;
+    let respostasCertas = document.querySelectorAll(".select.true").length;
+    
+    result = Math.round((respostasCertas/perguntasLength)*100);
+    // console.log(result);
+
+    const levels = saveData.levels
+    levels.forEach(level => {
+        let minValue = level.minValue;
+        if (result >= minValue) {
+            const quizResultado = document.querySelector(".resultado-final");
+            quizResultado.innerHTML = `
+            <div class="resultado-final__porcentagem" data-identifier="quizz-result">${result}% de acerto: ${level.title}</div>
+            <div class="resultado-final__imagem-e-parabenizacao">
+                <img src="${level.image}">
+                <p>${level.text}</p>
+            </div>
+            `;
+        }
+    });
+
+}
 
 function reniciarQuiz(){
-    
+    pass = 0;
+    //scrollar para o topo
+    const element = document.querySelector(".pagina-de-um-quizz__titulo-de-um-quizz");
+    element.scrollIntoView();
+    //cores das respostas volta a ser preto
+    let selectAnswer = document.querySelectorAll(".answer h4");
+    selectAnswer.forEach(h4 => {
+        h4.style.color = "black";
+    });
+    //remover o esbranquiçado
+    let removeBranco = document.querySelectorAll(".esbranquicado")
+    removeBranco.forEach(remove => {
+        remove.classList.remove("esbranquicado");
+    });
+    //adicionar atributo onclick e remover classe select
+    let addOnClick = document.querySelectorAll(".answer");
+    addOnClick.forEach(element => {
+        element.setAttribute("onclick", "escolheResposta(this)");
+        element.classList.remove("selecionado");
+        // console.log(element);
+    });
+    // (colocar parte aqui para esconder e atualizar resultado do quiz)
+    document.querySelector(".resultado-final").classList.add("esconder");
 }
 
-
 function voltarTelaInicial(){
+    voltarHome() 
+}
 
+function apareceBotoes(){
+    mostrarBotoes=document.querySelector(".aparece-botoes");
+    mostrarBotoes.classList.remove("esconder")
 }
